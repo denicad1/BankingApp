fb0727791c1c2c64ee726844bed5c6f131e1cba2
Anthony <denicad@msn.com>
2022-08-09 21:02:00 -0400

    start of banking app. only created transaction class properties so far.

---
+++ src/bankingApp/Main.java
@@ -0,0 +1,7 @@
+package bankingApp;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/bankingApp/Transaction.java
@@ -0,0 +1,10 @@
+package bankingApp;
+
+import java.util.Date;
+
+public class Transaction {
+    private Date date;
+    private String note;
+    private int transNumber;
+
+}
