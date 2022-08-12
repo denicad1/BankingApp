e1631afbb9d9aa3b46a333922b27ce640292fd18
Anthony <denicad@msn.com>
2022-08-12 12:33:58 -0400

    added create withdraw and deposit method to account class. working on those methods now.

--- src/bankingApp/Account.java
+++ src/bankingApp/Account.java
@@ -1,7 +1,9 @@
 package bankingApp;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Account {
     protected double balance=0;
@@ -15,5 +17,52 @@ public class Account {
     public Account(double balance, String name) {
         this.balance = balance;
         this.name = name;
+    }
+    //still need to configure createDeposit method. only copied from createWithdraw method
+    //so far
+    double createDeposit(){
+        Date date=new Date();
+        String note="";
+        double amount=0;
+        Scanner input=new Scanner(System.in);
+
+        try {
+
+            System.out.println("Why are you making a withdraw?");
+            note =input.nextLine();
+        }catch (Exception e){
+            System.out.println("Let's try that again. Why are you making a withdraw? ");
+        }
+        try {
+            System.out.println("How much is your withdraw?");
+            amount =input.nextDouble();
+        }catch (Exception e){
+            System.out.println("Please enter a number for the amount");
+        }
+        Withdraw withdraw= new Withdraw(date,note,this.transactions.size(),amount);
+//  need to finish minusing withdraw from balance as long as balance doesn't go below zero
+        return withdraw;
+    double createWithdraw(){
+        Date date=new Date();
+        String note="";
+        double amount=0;
+        Scanner input=new Scanner(System.in);
+
+        try {
+
+            System.out.println("Why are you making a withdraw?");
+            note =input.nextLine();
+        }catch (Exception e){
+            System.out.println("Let's try that again. Why are you making a withdraw? ");
+        }
+        try {
+            System.out.println("How much is your withdraw?");
+            amount =input.nextDouble();
+        }catch (Exception e){
+            System.out.println("Please enter a number for the amount");
+        }
+        Withdraw withdraw= new Withdraw(date,note,this.transactions.size(),amount);
+//  need to finish minusing withdraw from balance as long as balance doesn't go below zero
+        return withdraw;
     }
 }
--- src/bankingApp/Main.java
+++ src/bankingApp/Main.java
@@ -37,29 +37,7 @@ public class Main {
         }
         return choice;
     }
-    double createWithdraw(){
-        Date date=new Date();
-        String note="";
-        double amount=0;
-        Scanner input=new Scanner(System.in);
 
-        try {
-
-            System.out.println("Why are you making a withdraw?");
-            note =input.nextLine();
-        }catch (Exception e){
-            System.out.println("Let's try that again. Why are you making a withdraw? ");
-        }
-        try {
-            System.out.println("How much is your withdraw?");
-            amount =input.nextDouble();
-        }catch (Exception e){
-            System.out.println("Please enter a number for the amount");
-        }
-        Withdraw withdraw= new Withdraw(date,note,balance.transactions.size(),amount);
-        balance.balance-=
-//  need to finish minusing withdraw from balance as long as balance doesn't go below zero
-    }
 
 
 }
